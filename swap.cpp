a0e057093f2dd472a68f885f16b20ce74e5af376
laineywatlington <lainey.watlington@gmail.com>
2018-02-20 16:06:40 -0800

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void swap(int a, int b){
-  int tmp = a;
-  a = b;
-  b = tmp;
+void swap(int* a, int* b){ //a and b are pointers to integers
+  int tmp = *a; //*a will give you the value of x because a is pointing to x. *a means dereference a
+  *a = *b; //*b will give you value of y (20)
+  *b = tmp; //change wherever b is pointing to tmp 
 }
 
 int main(){
   int x = 10, y = 20;
   cout<< " x = "<<x<<" y = "<<y<<endl;
-  swap(x,y);
+  swap(&x,&y); //want to swap the address of a and b 
   cout<<" x = "<<x<<" y = "<<y<<endl;
 }
